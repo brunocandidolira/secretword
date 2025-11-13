1dd99454854616f523e778f4cbd756c04734f581
Bruno Candido <bruno.georgecandido@gmail.com>
2025-11-13 15:17:38 -0300

    feat: implementado a pasta components e criado o StartScreen.js e StartScreen.css

---
+++ src/Components/StartScreen.js
@@ -0,0 +1,13 @@
+import'./StartScreen.css';
+
+function StartScreen() {
+  return (
+    <div className='Start'>
+        <h1>Secret Word</h1>
+        <p>Click no Botão abaixo para começar a jogar</p>
+        <button>começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
