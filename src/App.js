2c9417555e2804ae1ce489a698062d33d5416bd7
Bruno Candido <bruno.georgecandido@gmail.com>
2025-11-17 09:42:06 -0300

    feat: implementado as de controller do jogo no APP.js , implementado  a funcao clearLetterStates e ultilizado  o useEffect  para verficar o estado do jogo.

--- src/App.js
+++ src/App.js
@@ -18,23 +18,31 @@ const  stages=[
   {id:2,name:"Game"},
   {id:3, name:"End"},
 ]
+const guessesQty=5;
 function App() {
 
 const [gameStage,setGameStage]= useState(stages[0].name);
-const [word]=useState(wordsList);
+const [words]=useState(wordsList);
 
 const [pickedWord,setPickedWord]=useState("");
 const [pickedCategory,setPickedCategory]=useState("");
 const [letters,setLetters]=useState([]);
+const [guessedLetters,setGuessedLetters]=useState([]);
+const [wrongLetters,setWrongLetters]=useState([]);
+const [guesses,setGuesses]=useState(guessesQty);
+const [score,setScore]=useState(0);
+
 
 const pickedWordandCategory=()=>{
   //pick a radom category
-const categories= Object.keys(word);
+const categories= Object.keys(words);
 const category=
-categories[Math.floor(Math.random()*Object.keys(word).length)];
+categories[Math.floor(Math.random()*Object.keys(categories).length)];
+console.log(category);
+
 //pick a radom word
-  const words= word [category][Math.floor(Math.random()*word[category].length)];
-  return {words,category}
+  const word= words[category][Math.floor(Math.random()*words[category].length)];
+  return {word,category}
 
 };
 
@@ -42,35 +50,75 @@ categories[Math.floor(Math.random()*Object.keys(word).length)];
 const StartGame=()=>{
 
 //pick word and pick category
- const {words,category} =pickedWordandCategory();
+ const {word,category} =pickedWordandCategory();
 
  //creat  an array of letters
- let wordLetters= words.split("");
+ let wordLetters= word.split("");
 
  wordLetters= wordLetters.map((l)=>l.toLowerCase());
-
+console.log(word,category);
+console.log(wordLetters);
  //fill states
- setPickedWord(words);
+ setPickedWord(word);
  setPickedCategory(category);
- setLetters(letters);
+ setLetters(wordLetters);
 
   setGameStage(stages[1].name);
 
 }
 
 //process  the letter input
-const verifyLetter = ()=>{
-    setGameStage(stages[2].name);
+const verifyLetter = (letter)=>{
+  
+  const normalizedLetter= letter.toLowerCase();
+
+  //check if letter has already been utilized
+  if(guessedLetters.includes(normalizedLetter)||wrongLetters.includes(normalizedLetter)){
+return;
+  }
+  //push guessed  letter or remove a guess
+if (letters.includes(normalizedLetter)){
+setGuessedLetters((actualGuessedLetters)=>[
+  ...actualGuessedLetters,normalizedLetter
+])
+}else{
+setWrongLetters((actualwrongLetters)=>[
+  ...actualwrongLetters,normalizedLetter
+])
+setGuesses((actualGuesses)=>actualGuesses -1)
 }
+};
+const clearLetterStates=()=>{
+  setGuessedLetters([])
+  setWrongLetters([])
+}
+
+useEffect(() => {
+  if (guesses <= 0) {
+    clearLetterStates();
+    setGameStage(stages[2].name);
+  }
+}, [guesses]);  // <-- fecha só o useEffect
+
 //restarts the game
 const retry=()=>{
+  setScore(0)
+  setGameStage(guessesQty)
   setGameStage(stages[0].name);
 }
   return (
     <div className="App">
 
    {gameStage === 'Start' && <StartScreen StartGame={StartGame}/>}
-   {gameStage === 'Game' && <Game verifyLetter={verifyLetter}/>}
+   {gameStage === 'Game' && <Game verifyLetter={verifyLetter}
+   pickedWord={pickedWord}
+   pickedCategory={pickedCategory}
+   letters={letters}
+   guessedLetters={guessedLetters}
+   wrongLetters={wrongLetters}
+   guesses={guesses}
+   score={score}
+   />}
    {gameStage === 'End'&& <GameOver retry={retry}/>}
 
       
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,33 +1,58 @@
 import './Game.css';
+import { useState,useRef } from 'react';
+function Game({verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
 
-function Game({verifyLetter}) {
+}) {
+ const[letter,setLetter]=useState("")
+const letterInputRef= useRef(null);
+
+
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  verifyLetter(letter);
+  setLetter("");
+  letterInputRef.current.focus();
+  
+};
+
+
   return (
     <div className="game">
       <p className="points">
 
-       <span>pontuação: 000</span>
+        <span>pontuação: {score}</span>
       </p>
        <h1>Advinhe a palavra: </h1>
        <h3 className="tip"> 
-      Dica sobre a palavra:  <span> Dica...</span>
+      Dica sobre a palavra:  <span>{pickedCategory}</span>
        </h3>
+       <p>você ainda tem {guesses} tentativa(s).</p>
        <div className="wordContainer">
-        <span className="letter"> A</span>
-        <span className="blanckSquare"></span>
+        {letters.map((letter,i) =>
+guessedLetters.includes(letter) ?( <span key={i} className="letter">{letter}</span>                                 
+):( 
+<span key={i} className="blankSquare"></span>                                 
+
+     ) ) }
        </div>
        <div className="letterContainer">
        <p>Tente Advinhar uma letra da palavra:</p>
-       <form>
+       <form onSubmit={handleSubmit}>
 
-        <input type="text" name="letter" maxLength="1" required />
+        <input type="text" name="letter" maxLength="1" required onChange={(e)=>setLetter(e.target.value)} value={letter} ref={letterInputRef}/>
         <button>Jogar!</button>
        </form>
        </div>
        <div className="wrongLettersContainer">
         <p>Letras já utilizadas:</p>
-        <span>a,</span>
-        <span>b,</span>
-
+       {wrongLetters.map((letter,i)=>(<span key={i}>{letter},</span>))}
        </div>
     </div>
   );
